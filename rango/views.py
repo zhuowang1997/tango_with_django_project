8a26754eee471212db585cd1a8ac90f64874cf2e
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-07-27 11:07:09 +0800

    exch3

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!" + "http://127.0.0.1:8000/rango/about/")
+    return HttpResponse("Rango says hey there partner!" + "<a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page." + "http://127.0.0.1:8000/rango/index/")
+    return HttpResponse("Rango says here is the about page." + "<a href='/rango/'>Index</a>")
