1101c4b8ce854f2778faa132624e5650633d85c3
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-07-27 10:37:22 +0800

    create a view

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
