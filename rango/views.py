d997705e0df81850c61077fe596072c3d231cb07
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-07-27 11:05:15 +0800

    exch3

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner!" + "http://127.0.0.1:8000/rango/about/")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page." + "http://127.0.0.1:8000/rango/index/")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,6 +20,7 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.about, name='about'),
     path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
