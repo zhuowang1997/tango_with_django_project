601bc1fccdfbcc06512489dd2faf2bdb26068c36
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-07-27 22:53:20 +0800

    database

--- rango/models.py
+++ rango/models.py
@@ -11,6 +11,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.title
