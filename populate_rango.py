9cd796024f6b90edc3f77e9ab4a95fba2cc19930
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-07-28 10:37:20 +0800

    exch5

--- populate_rango.py
+++ populate_rango.py
@@ -45,7 +45,7 @@ def populate():
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat,views=cat_data['views'],likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
