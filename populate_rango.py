fa229d38681387e3ddc6f8b0d36f58de3e61546f
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-07-28 10:35:36 +0800

    exch5

--- populate_rango.py
+++ populate_rango.py
@@ -35,9 +35,9 @@ def populate():
         {'title':'Flask',
         'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages},
-        'Django': {'pages': django_pages},
-        'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages,'views':128,'likes':64},
+        'Django': {'pages': django_pages,'views':64,'likes':32},
+        'Other Frameworks': {'pages': other_pages,'views':32,'likes':16} }
 
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
@@ -63,6 +63,8 @@ def add_page(cat, title, url, views=0):
 
 def add_cat(name,views=0,likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
