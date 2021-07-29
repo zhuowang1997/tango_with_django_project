064734c8a2caf444671b10f09d20530dfc7259ed
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-07-29 14:28:40 +0800

    login

--- rango/urls.py
+++ rango/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('category/<slug:category_name_slug>/add_page/',
         views.add_page, name='add_page'),
     path('register/', views.register, name='register'), 
+    path('login/', views.user_login, name='login'),
 ]
