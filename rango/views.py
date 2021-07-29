f574998975f8450a8f17558a63794c0510ee1dff
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-07-29 14:55:01 +0800

    logout

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -198,3 +198,13 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+
+# Use the login_required() decorator to ensure only those logged in can
+# access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return redirect(reverse('rango:index'))
