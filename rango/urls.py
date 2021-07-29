96e056969983fd445657c749600aa429b436f3c7
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-07-29 14:20:49 +0800

    chapter9

--- rango/urls.py
+++ rango/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('add_category/', views.add_category, name='add_category'),
     path('category/<slug:category_name_slug>/add_page/',
         views.add_page, name='add_page'),
-
+    path('register/', views.register, name='register'), 
 ]
